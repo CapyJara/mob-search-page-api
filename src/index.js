04ee274f95cb02005e4210f69babdb9ddb69257d
Jared Myhrberg <jared.myhrberg@gmail.com>
2019-03-05 14:38:40 -0800

    removed paging files and paging function calls

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import { loadMovies } from './load-movie.js';
 import { movies } from '../data/sample-data.js';
-import loadPaging from './paging.js';
 import './search-component.js';
 
 loadMovies(movies);
-loadPaging(movies.length);
