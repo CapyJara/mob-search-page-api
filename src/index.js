386cf65eb1b5efec9e2a8cd55d9a5364742d9b82
Jared Myhrberg <jared.myhrberg@gmail.com>
2019-03-05 14:37:51 -0800

    url updating based on user input

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { loadMovies } from './load-movie.js';
 import { movies } from '../data/sample-data.js';
 import loadPaging from './paging.js';
+import './search-component.js';
 
 loadMovies(movies);
 loadPaging(movies.length);
